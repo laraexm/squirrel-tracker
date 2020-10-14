caa24db0687efbca5c87763706f8e9ef568cfb6a
Xiaomeng E <xe2000@columbia.edu>
2020-10-14 20:07:15 +0000

    edited import cmd and models

--- app/management/commands/import_squirrel_data.py
+++ app/management/commands/import_squirrel_data.py
@@ -15,6 +15,34 @@ class Command(BaseCommand):
 
         with open(file_) as fp:
             reader = csv.DictReader(fp)
+            
+            for item in reader:
+                obj = Squirrel()
+                obj.X_Latitude = item['X_Latitude']
+                obj.Y_Longitude = item['Y_Longitude']
+                obj.Unique_Squirrel_ID = item['Unique_Squirrel_ID']
+                obj.Shift = item['Shift']
+                obj.Date = item['Date']
+                obj.age = item['age']
+                obj.Primary_Fur_Color = item['Primary_Fur_Color']
+                obj.Location = item['Location']
+                obj.Specific_Location = item['Specific_Location']
+                obj.Running = item['Running']
+                obj.Chasing = item['Chasing']
+                obj.Climbing = item['Climbing']
+                obj.Eating = item['Eating']
+                obj.Foraging = item['Foraging']
+                obj.Other_Activity = item['Other_Activity']
+                obj.Kuks = item['Kuks']
+                obj.Quaas = item['Quaas']
+                obj.Moans = item['Moans']
+                obj.Tail_Flags = item['Tail_Flags']
+                obj.Tail_Twitches = item['Tail_Twitches']
+                obj.Approaches = item['Approaches']
+                obj.Indifferent = item['Indifferent']
+                obj.Runs_From = item['Runs_From']
+
+                obj.save()
 
         msg = f'you are importing from {file_}'
 
