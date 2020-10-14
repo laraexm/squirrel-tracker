8257fbf55ce7711303f1f541a3714b905337764c
Xiaomeng E <xe2000@columbia.edu>
2020-10-14 23:21:15 +0000

    under /sightings/ list of ids

--- app/views.py
+++ app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Squirrel
+
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+def index(request):
+    squirrels = Squirrel.objects.all()
+    context = {
+            'squirrels': squirrels,
+            }
+    return render(request, 'app/index.html',context)
+
+
