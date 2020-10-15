af6b1cf0fa9e84cad12073312168aeb41c3cf0fe
Xiaomeng E <xe2000@columbia.edu>
2020-10-15 23:56:59 +0000

    added map

--- project/urls.py
+++ project/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sightings/', include('app.urls')),
+    path('map/', include('app.mapurl')),
     ]
