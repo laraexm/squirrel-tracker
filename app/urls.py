a98de37d218f89d371452a2d0053cae1a1b5887a
chilly-wang <sw3506@columbia.edu>
2020-10-18 04:19:27 +0000

    Modified Detail view and related codes. Make unique ID primary key.

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ app_name = 'app'
 
 urlpatterns = [
         path('sightings/', views.index),
-        path('<int:squirrel_id>/', views.detail, name='detail'),
+        path('<Unique_Squirrel_ID>/', views.detail, name='detail'),
         path('map/', views.map),
         path('sightings/add/', views.create_new_sighting, name='Create New Sighting'),
         path('sightings/<Unique_Squirrel_ID>/', views.update_sighting, name='Update Sighting'),
