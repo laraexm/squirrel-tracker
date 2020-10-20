07c58ac880cc7a691493c02c3ffa661965df0012
Xiaomeng E <xe2000@columbia.edu>
2020-10-20 00:59:31 +0000

    removed duplicates  Please enter the commit message for your changes. Lines starting

--- app/management/commands/import_squirrel_data.py
+++ app/management/commands/import_squirrel_data.py
@@ -18,36 +18,41 @@ class Command(BaseCommand):
         with open(file_) as fp:
             reader = csv.DictReader(fp)
             
+            Unique_ID_List = []
             for item in reader:
-                obj = Squirrel()
-                obj.X_Latitude = item['X']
-                obj.Y_Longitude = item['Y']
-                obj.Unique_Squirrel_ID = item['Unique Squirrel ID']
-                obj.Shift = item['Shift']
-                raw = item['Date']
-                date_str = raw[-4:]+'-'+ raw[:2]+'-'+raw[2:4]
-                date = datetime.datetime.strptime(date_str, '%Y-%m-%d') 
-                obj.Date = date
-                obj.Age = item['Age']
-                obj.Primary_Fur_Color = item['Primary Fur Color']
-                obj.Location = item['Location']
-                obj.Specific_Location = item['Specific Location']
-                obj.Running = str(item['Running']) == 'TRUE'
-                obj.Chasing = str(item['Chasing']) == 'TRUE'
-                obj.Climbing = str(item['Climbing']) == 'TRUE'
-                obj.Eating = str(item['Eating']) == 'TRUE'
-                obj.Foraging = str(item['Foraging']) == 'TRUE'
-                obj.Other_Activity = str(item['Other Activities']) == 'TRUE'
-                obj.Kuks = str(item['Kuks']) == 'TRUE'
-                obj.Quaas = str(item['Quaas']) == 'TRUE'
-                obj.Moans = str(item['Moans']) == 'TRUE'
-                obj.Tail_Flags = str(item['Tail flags']) == 'TRUE'
-                obj.Tail_Twitches = str(item['Tail twitches']) == 'TRUE'
-                obj.Approaches = str(item['Approaches']) == 'TRUE'
-                obj.Indifferent = str(item['Indifferent']) == 'TRUE'
-                obj.Runs_From = str(item['Runs from']) == 'TRUE'
+                if item['Unique Squirrel ID'] not in Unique_ID_List:
+                    Unique_ID_List.append(item['Unique Squirrel ID'])
+                    obj = Squirrel()
+                    obj.X_Latitude = item['X']
+                    obj.Y_Longitude = item['Y']
+                    obj.Unique_Squirrel_ID = item['Unique Squirrel ID']
+                    obj.Shift = item['Shift']
+                    raw = item['Date']
+                    date_str = raw[-4:]+'-'+ raw[:2]+'-'+raw[2:4]
+                    date = datetime.datetime.strptime(date_str, '%Y-%m-%d') 
+                    obj.Date = date
+                    obj.Age = item['Age']
+                    obj.Primary_Fur_Color = item['Primary Fur Color']
+                    obj.Location = item['Location']
+                    obj.Specific_Location = item['Specific Location']
+                    obj.Running = str(item['Running']) == 'TRUE'
+                    obj.Chasing = str(item['Chasing']) == 'TRUE'
+                    obj.Climbing = str(item['Climbing']) == 'TRUE'
+                    obj.Eating = str(item['Eating']) == 'TRUE'
+                    obj.Foraging = str(item['Foraging']) == 'TRUE'
+                    obj.Other_Activity = str(item['Other Activities']) == 'TRUE'
+                    obj.Kuks = str(item['Kuks']) == 'TRUE'
+                    obj.Quaas = str(item['Quaas']) == 'TRUE'
+                    obj.Moans = str(item['Moans']) == 'TRUE'
+                    obj.Tail_Flags = str(item['Tail flags']) == 'TRUE'
+                    obj.Tail_Twitches = str(item['Tail twitches']) == 'TRUE'
+                    obj.Approaches = str(item['Approaches']) == 'TRUE'
+                    obj.Indifferent = str(item['Indifferent']) == 'TRUE'
+                    obj.Runs_From = str(item['Runs from']) == 'TRUE'
 
-                obj.save()
+                    obj.save()
+                else:
+                    pass
 
         msg = f'you are importing from {file_}'
 
